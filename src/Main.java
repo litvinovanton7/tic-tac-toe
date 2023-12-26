bb5fa12ee3202ed7d8d2c6fd9b6de9094ba4e7a8
litvinovanton7 <antonafonin154@gmail.com>
2023-12-26 11:17:31 -0500

    feat: file Main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// player always is first
+// player always use X
+// bot O
+// bot chooses random empty cell
+// without OOP
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
